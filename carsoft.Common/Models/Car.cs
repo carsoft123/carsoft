aab04c52583315ddfd53b6e220149863ec84445e
calos olivares <carlos.olivares.s@hotmail.com>
2021-12-10 21:34:47 -0500

    Add Entities

---
+++ carsoft.Common/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace carsoft.Common.Models
+{
+    internal class Car
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ carsoft.Functions/Entities/CarEntities.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carsoft.Functions.Entities
+{
+    public class CarEntities : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
